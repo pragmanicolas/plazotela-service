ee6f6d37f3c7777f4de21bcf4750646a7707dbad
Nicolas Santillan <nicolas.santillan@pragma.com.co>
2024-09-12 00:11:34 -0500

    feat update pom.xml, SecurityConfig y application.properties

--- src/main/java/com/plazoleta/config/SecurityConfig.java
+++ src/main/java/com/plazoleta/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.plazoleta.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -24,39 +25,11 @@ public class SecurityConfig {
                 .authorizeHttpRequests(requests -> requests
                         .requestMatchers("/publico/**").permitAll()
                         .requestMatchers("/actuator/health").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/detalles/**").authenticated()
-                        .requestMatchers(HttpMethod.POST, "/detalles/**").hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.GET,"/platos/**").authenticated()
-                        .requestMatchers(HttpMethod.GET,"/restaurantes/**").authenticated()
-                        .requestMatchers(HttpMethod.POST, "/platos/**").hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.POST, "/restaurantes/**").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 )
-                .oauth2Login(withDefaults())
-                .httpBasic(withDefaults());
+                .oauth2Login(Customizer.withDefaults());
 
         return http.build();
     }
 
-    @Bean
-    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder){
-        UserDetails user = User.builder()
-                .username("user")
-                .password(passwordEncoder.encode("password"))
-                .roles("USER")
-                .build();
-
-        UserDetails admin = User.builder()
-                .username("admin")
-                .password(passwordEncoder.encode("adminpassword"))
-                .roles("ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(user,admin);
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
-    }
 }
